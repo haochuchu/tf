15302e4bd01ab58af0536655758a82b9a5c34bf7
guohaojing <13663677607@qq.com>
2017-08-03 08:43:20 +0800

    8.3

--- routes/img.js
+++ routes/img.js
@@ -12,12 +12,51 @@ var pool=mysql.createPool({
 	database:'tianfang',
 	port:3306
 })
-router.post('/img',function(req,res){
+router.get('/img',function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+pool.query('select * from img',function(err,rows){
+		if(err) throw err;
+		res.send(rows);
+	})
+})
+
+
+//修改
+router.post('/upprize12',function(req,res){
+	var id=req.body["id"]
+	res.header("Access-Control-Allow-Origin", "*");
+pool.query(`update img set  img="${imgs}" where id=${id}`, function(err, rows, fields) {
+		if (err) throw err;
+	  	res.send("修改成功")
+	});
+})
+//删除
+router.post('/dlcases12',function(req,res){
+	var id=req.body["id"]
+	var title=req.body["title"]
+	res.header("Access-Control-Allow-Origin", "*");
+	pool.query(`delete from img where id=${id}`, function(err, rows, fields) {
+		if (err) throw err;
+	  	res.send("删除成功")
+	});
+})
+//插入
+router.post('/accases12',function(req,res){
+	var text=req.body["text"]
+	res.header("Access-Control-Allow-Origin", "*");
+	pool.query(`insert into img(img) values("${imgs}")`,function(err,rows){
+			if (err) throw err;
+			if(rows){
+				res.send("上传成功")
+			}
+			
+		})
+})
+router.post('/incases12',function(req,res){
 	res.header("Access-Control-Allow-Origin", "*"); //跨域
 	var form = new formidable.IncomingForm();
 	form.uploadDir='public/images';
 	  //上传图片存放的路径
-	
 	form.parse(req,function(error,fields,files){
 		for(var i in files){
 			var file = files[i];  //保存图片属性
@@ -35,28 +74,19 @@ router.post('/img',function(req,res){
 			}
 			var newPath='public/images/'+fName;  //要返回的图片的路径
 			fs.renameSync(file.path,newPath);
-			 // res.send(fName)
+			  res.send(newPath)
 		}
-
-
-
-		pool.query(`insert into img(img) values('http://localhost:8100/images/${fName}')`,function(err,rows){
-			if (err) throw err;
-			if(rows){
-				res.send('上传成功')
-			}
-		})
+		imgs=`http://localhost:8100/images/${fName}`
 	})
 	});
-
-
 //调取图片
-router.get('/photo',function(req,res){
+router.post('/alcases12',function(req,res){
+	var id=req.body["id"]
 	res.header("Access-Control-Allow-Origin", "*");
-pool.query('select * from img',function(err,rows){
-
-		if(err) throw err;
-		res.send(rows);
-	})
+pool.query(`update img set  img="${imgs}" where id=${id}`, function(err, rows, fields) {
+		if (err) throw err;
+	  	res.send("修改成功")
+	});
 })
+
 module.exports=router;
